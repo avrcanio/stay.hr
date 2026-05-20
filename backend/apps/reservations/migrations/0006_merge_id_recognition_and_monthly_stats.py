40f4c238977711598a367df932f71b5c70a6a4f5
Ante Vrcan <avrcanus@gmail.com>
2026-05-20 20:01:49 +0200

    merge

---
+++ backend/apps/reservations/migrations/0006_merge_id_recognition_and_monthly_stats.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("reservations", "0004_id_recognition_sample"),
+        ("reservations", "0005_monthly_statistics_override"),
+    ]
+
+    operations = []
